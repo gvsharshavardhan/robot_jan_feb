b5fef441a9c976963f3bf9030342964163818e84
Harsha Vardhan <gvsharsha6316@gmail.com>
2021-01-31 18:51:48 +0530

    notes

--- selenium_proj/name.py
+++ selenium_proj/name.py
@@ -1,2 +1,19 @@
 def pythonm():
     return "this is python class"
+
+
+# ui - slenium - bdd (cucumber)
+#
+# testng, pytest
+# cucumber
+#
+# java selenium cucumber senerity serenity rest locust postman browser stack
+#
+# back end - rest api - request, rest assured - bdd
+#
+# mobile - appium
+
+
+
+
+
